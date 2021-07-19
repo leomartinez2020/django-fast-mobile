3f4d004714620dfdcfc31f183a4dda5d0bbaee1c
Leonardo Martinez <lowenhard@yahoo.com>
2021-07-19 14:40:26 -0500

    Update settings.py

--- houserent/settings.py
+++ houserent/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(3$hrh8z31h7r(-5uzmct=3#=1x=9-+q47iua_&p(^rf=@j8_t'
+SECRET_KEY = os.environ.get('MYSECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
