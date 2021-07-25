24a6d8576a835138a46c43228df425b6c9ae4cb5
leomartinez2020 <lowenhard@yahoo.com>
2021-07-25 20:57:02 +0000

    Improve security

--- fastmobile/settings.py
+++ fastmobile/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('MYSECRET')
-SECRET_KEY = 'django123fastmobile'
+envfile = os.path.join(BASE_DIR, "myconfig/mykey.txt")
+with open(envfile) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == 'dev' else False
